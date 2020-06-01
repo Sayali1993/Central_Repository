cdbdbf85d3673697f9df2e1dd69761c437439dc2
sayali1993 <sayali.nakade@spryiq.co>
2020-06-01 22:49:29 +0530

    Save Assignment_6_done

--- Assignment_6_done.py
+++ Assignment_6_done.py
@@ -1,17 +1,19 @@
 import os
-txt, pdf = ([] for i in range(2))
+
 
 list_files = ["file1.txt", "file1.pdf", "file1.xlsx", "file1.xls", "file2.txt", "file3.pdf", "file4.mp4", "file2.ppt.txt"]
 print("\nList of files : ", list_files)
 print("-------------\n")
 
-def sorting_list(lst):
 
+def sorting_list(lst):
+    ext_lis = []
+    dic = {}
     for file_ in lst:
         name, ext1 = os.path.splitext(file_)
-        txt.append(ext1)
-        mylist = list(set(txt))
-        dic = {}
+        ext_lis.append(ext1)
+        mylist = list(set(ext_lis))
+
     for i in range(0, len(mylist)):
         ll = []
         for file1 in lst:
@@ -23,6 +25,7 @@ def sorting_list(lst):
 
     print('Output : ', dic)
     return dic
+
 
 sorting_list(list_files)
 
