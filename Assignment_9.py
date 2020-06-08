f4c411068d5d60943d6db7a73da7303828339c7b
sayali1993 <sayali.nakade@spryiq.co>
2020-06-08 23:10:35 +0530

    save assignment 9

--- Assignment_9.py
+++ Assignment_9.py
@@ -1,20 +1,30 @@
 import os.path
 import shutil
+
 path = input('\nEnter the path [e.g, C:/..] to create files : ')
 isExist = os.path.exists(path)
-if isExist:
-    no_of_files = int(input("Enter number of files you want to create: "))
-    total_size = no_of_files * 2048
-    stat = shutil.disk_usage(path)
-    if total_size > stat[2]:
-        print("* No sufficient memory *")
+while True:
+    if isExist:
+        try:
+            no_of_files = int(input("Enter number of files you want to create: "))
+            total_size = no_of_files * 2048
+            stat = shutil.disk_usage(path)
+            if total_size > stat[2]:
+                print("* No sufficient memory *")
+            else:
+                for i in range(no_of_files):
+                    file_name = (path + "file_" + str(i) + ".txt")
+                    file = open(file_name, "w")
+                    string = "S" * 2048
+                    file.write(string)
+                    file.close()
+                print("\n* ", no_of_files, " files are successfully created at location", path)
+                print('* Each file has data size 2048 char string *')
+            break
+        except:
+            print("* ERROR -> Please enter the valid input [E.g, 2]. *\n")
     else:
-        for i in range(no_of_files):
-            file_name = (path + "file_" + str(i) + ".txt")
-            file = open(file_name, "w")
-            string = "S" * 2048
-            file.write(string)
-            file.close()
-        print("* ", no_of_files, " files are successfully created at location", path)
-else:
-    print("Path not exist. Please enter the valid path to create files.")
+        print("* ERROR -> Path not exist. Please enter the valid path to create files. *\n")
+        break
+
+
