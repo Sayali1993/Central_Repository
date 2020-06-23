7074e8274913a76b88415f6f54e3c379534364e2
sayali1993 <sayali.nakade@spryiq.co>
2020-06-24 00:55:38 +0530

    Save  Assignment 2 :Display data from your timeline.

---
+++ Twitter_assignment3.py
@@ -0,0 +1,35 @@
+import tweepy
+
+
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_data(self, raw_data):
+        self.process_data(raw_data)
+
+        return True
+
+    def process_data(self, raw_data):
+        print(raw_data)
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            # returning False in on_error disconnects the stream
+            return False
+
+
+class MaxStream():
+    def __init__(self, auth, listener):
+        self.stream = tweepy.Stream(auth=auth, listener=listener)
+
+    def start(self):
+        self.stream.filter(track=['Time'])
+
+
+
+if __name__ == "__main__":
+    listener = MyStreamListener()
+    auth = tweepy.OAuthHandler("consumer_key", "consumer_secret")
+    auth.set_access_token("access_token", "access_token_secret")
+
+    stream = MaxStream(auth, listener)
+    stream.start()
