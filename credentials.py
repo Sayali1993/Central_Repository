9adef052fa4921d931bd7beec707e4965cd54c2c
sayali1993 <sayali.nakade@spryiq.co>
2020-06-26 18:46:59 +0530

    Structured Assignmnet

---
+++ credentials.py
@@ -0,0 +1,6 @@
+consumer_key = 'XXXXXXXXXXXXXXX'
+consumer_secret = 'XXXXXXXXXXXXXXXXX'
+access_token = 'XXXXXXXXXXXXXXXXXXXXXXXX'
+access_token_secret = 'XXXXXXXXXXXXXXXXXx'
+numberOfTweets = 3
+user_name = "Screen_name"
